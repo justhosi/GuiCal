a1f11256b58f9fb1d74474eff0c3ae437bad4295
justhosi <44212529+justhosi@users.noreply.github.com>
2023-11-29 11:48:22 +0300

    Adding power and square root to the functions of the calculator

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
+from math import sqrt
 
+
+#For showing error when devide by zero
 def show_error():
     messagebox.showerror("Devision by zero", "You cannot devide by zero")
 
@@ -25,6 +28,14 @@ def devide():
     else:
         awnser = float(input_1.get()) / float(input_2.get())
         show_result.config(text=awnser, fg= "white")
+
+def power(): 
+    awnser = float(input_1.get()) ** float(input_2.get())
+    show_result.config(text=awnser)
+
+def square_root():
+    awnser = sqrt(float(input_1.get()))
+    show_result.config(text=awnser)
 
 windows = Tk()
 windows.title("Calculator")
@@ -53,12 +64,20 @@ btn_devide = Button(windows, text="÷", command=devide)
 btn_devide.pack()
 btn_devide.place(x=400, y=110)
 
+btn_power = Button(windows, text="^", command=power)
+btn_power.pack()
+btn_power.place(x=200, y=150)
+
+btn_squar_root= Button(windows, text="√", command=square_root)
+btn_squar_root.pack()
+btn_squar_root.place(x=300, y=150)
+
 result_label = Label(windows, text="Result:")
 result_label.pack()
-result_label.place(x=250, y=150)
+result_label.place(x=250, y=250)
 show_result = Label(windows, text="",font=('ubuntu', 28), bg="grey", width=18)
 show_result.pack()
-show_result.place(x=70, y=170)
+show_result.place(x=70, y=270)
 
 
 
